4a1acad26bd9b37cd8c7d0bd0b0661ef983d4829
Aly <alyashour1@gmail.com>
2024-03-22 00:50:26 -0400

    Whats this? New animations 😱😱😳

---
+++ Assets/Global/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Global.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        private void OnFire(InputValue inputValue)
+        {
+        
+        }
+    }
+}
--- Assets/Global/Player/PlayerMovement.cs
+++ Assets/Global/Player/PlayerMovement.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,44 +6,92 @@ using UnityEngine.InputSystem;
     Author: Aiden
 */
 
-public class PlayerMovement : MonoBehaviour
+namespace Global.Player
 {
-
-    // Component references
-    [SerializeField] Rigidbody2D rb;
-    [SerializeField] SpriteRenderer spriteRenderer;
-
-    // Store the current user input, and the current smoothed input
-    private Vector2 movementInput;
-    private Vector2 smoothedMovementInput;
-    // Place holder struct ref used in smooth dampening
-    private Vector2 movementInputSmoothedVelocity;
-
-    // The player speed
-    [SerializeField] float speed = 1.75f;
-    // The amount of time it takes to smooth the movement (basically how slippy the movement feels)
-    private float smoothTime = 0.05f;
-
-    private void FixedUpdate()
+    public class PlayerMovement : MonoBehaviour
     {
-        // Moves the player
-        Move();
+        // Component references
+        private Animator _animator;
+        private Rigidbody2D _rb;
 
+        // Store the current user input, and the current smoothed input
+        private Vector2 _movementInput;
+        private Vector2 _smoothedMovementInput;
+
+        // Place holder struct ref used in smooth dampening
+        private Vector2 _movementInputSmoothedVelocity;
+
+        // The player speed
+        [SerializeField] private float speed = 1.75f;
+
+        // The amount of time it takes to smooth the movement (basically how slippy the movement feels)
+        private const float SmoothTime = 0.05f;
+
+        private void Start()
+        {
+            _animator = gameObject.GetComponent<Animator>();
+            _rb = gameObject.GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            
+        }
+        
+        private void FixedUpdate()
+        {
+            // Moves the player
+            Move();
+        }
+
+
+        // Applies the player movement, based on the user input
+        private void Move()
+        {
+            // Calculates the new smoothed input value based on the current value and the target value
+            _smoothedMovementInput = Vector2.SmoothDamp(_smoothedMovementInput, _movementInput,
+                ref _movementInputSmoothedVelocity, SmoothTime);
+
+            // pull the movement input down to 0 if very close to avoid floating point shenanigans
+            _smoothedMovementInput = new Vector2(
+                Mathf.Abs(_smoothedMovementInput.x) < 0.2 ? 0f : _smoothedMovementInput.x,
+                Mathf.Abs(_smoothedMovementInput.y) < 0.2 ? 0f : _smoothedMovementInput.y
+            );
+            
+            // calculate and update velocity
+            _rb.velocity = _smoothedMovementInput * speed;
+            
+            // update animator
+            _animator.SetFloat("hSpeed", _rb.velocity.x);
+            _animator.SetFloat("vSpeed", _rb.velocity.y);
+        }
+
+        // Gets the user input to update the player movement vector
+        private void OnMove(InputValue inputValue)
+        {
+            var newMovementInput = inputValue.Get<Vector2>();
+
+            if (newMovementInput.magnitude < 0.05) // should be a very small number (bcuz floats)
+            {
+                _animator.SetBool("isMoving", false);
+                newMovementInput = Vector2.zero;
+            }
+            else
+            {
+                // the player is moving
+                _animator.SetBool("isMoving", true);
+                
+                var flip = newMovementInput.x < 0;
+                
+                gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, flip ? 180f : 0f));
+            }
+
+            _movementInput = newMovementInput;
+        }
+
+        private void OnFire(InputValue inputValue)
+        {
+            _animator.SetTrigger("Attack");
+        }
     }
-
-
-    // Applies the player movement, based on the user input
-    private void Move()
-    {
-        // Calculates the new smoothed input value based on the current value and the target value
-        smoothedMovementInput = Vector2.SmoothDamp(smoothedMovementInput, movementInput, ref movementInputSmoothedVelocity, smoothTime);
-        rb.velocity = smoothedMovementInput * speed;
-    }
-
-    // Gets the user input to update the player movement vector
-    private void OnMove(InputValue inputValue)
-    {
-        movementInput = inputValue.Get<Vector2>();
-    }
-
 }
