3354ff3d9c2a29f955891021ea65e24925c28232
Aiden <apalecz2@uwo.ca>
2024-04-02 20:10:28 -0400

    Made general fixes and win state to end the game

--- Assets/Scene Assets/FinalBoss/RingDrop/RingBehaviour.cs
+++ Assets/Scene Assets/FinalBoss/RingDrop/RingBehaviour.cs
@@ -5,8 +5,27 @@ using UnityEngine;
 public class RingBehaviour : MonoBehaviour
 {
     [SerializeField] float amplitude;
+
+    private SceneInitialization _sceneInitialization;
+    private void Start()
+    {
+        _sceneInitialization = GameObject.Find("SceneController").GetComponent<SceneInitialization>();
+    }
     void Update()
     {
         transform.position = new Vector3(transform.position.x, transform.position.y + Mathf.Sin(Time.time) * amplitude, transform.position.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            _sceneInitialization.sceneState = 10;
+            Destroy(gameObject, 0.5f);
+        }
+        
+
+
+
     }
 }
